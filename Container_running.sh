65a40c38b16644035f07e22300638b2f3f2b356b
loskyertt <loskyertt0403@gmail.com>
2024-12-12 11:30:53 +0800

    add qt-container

--- Container_running.sh
+++ Container_running.sh
@@ -27,12 +27,13 @@ docker run -it \
     --device=/dev/dri \
     stateoftheartio/qt6:6.8-gcc-aqt
 
-# 需要在容器中的设置
-
-# CMakeLists.txt 中需要添加：
-set(CMAKE_PREFIX_PATH "/opt/Qt/6.8.0/gcc_64")
-
 # 需要安装 OpenGL
 sudo apt install mesa-common-dev libgl1-mesa-dev
+
+# 如果在构建时出错，则需要在容器中的 CMakeLists.txt 添加：
+set(CMAKE_PREFIX_PATH "/opt/Qt/6.8.0/gcc_64")
+# 或者
+set(Qt_dir "/opt/Qt/6.8.0/gcc_64/lib/cmake/Qt6")
+find_package(Qt6 REQUIRED COMPONENTS Core Widgets HINTS ${Qt_dir})
 
 ## -----------------------------------------------------------------------------------
