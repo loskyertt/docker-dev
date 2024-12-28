13fbbae5a236d0002ee746d2f8fd138387f940cb
loskyertt <loskyertt0403@gmail.com>
2024-12-28 14:11:23 +0800

    change qt

--- qt/run_qt.sh
+++ qt/run_qt.sh
@@ -9,11 +9,25 @@ docker run -it \
     --device=/dev/dri \
     stateoftheartio/qt6:6.8-gcc-aqt
 
-# 需要在容器内安装 OpenGL
-sudo apt install mesa-common-dev libgl1-mesa-dev
+
+## ------------------------------------------------ 安装必要包 ----------------------------------------------------------
+# 更新
+sudo apt update
+sudo apt upgrade
+
+# 需要在容器内安装 OpenGL 和中文字体
+sudo apt install -y mesa-common-dev libgl1-mesa-dev fonts-noto-cjk
 
 # wayland 需要
-apt install -y weston
+sudo apt install -y weston
+
+# 通过访问权限
+xhost +local:docker 
+
+# 若要使用 clangd（注意版本） 语法服务
+sudo apt install -y clangd-18
+# 然后创建链接
+sudo ln -s /usr/bin/clangd-18 /usr/bin/clangd
 
 
 ## ------------------------------------------------ CMakeLists.txt 配置 ----------------------------------------------------------
