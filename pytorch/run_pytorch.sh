f0ae5ceda3e6ef378a0312bb9c24055747a62e49
loskyertt <loskyertt0403@gmail.com>
2024-12-28 12:42:52 +0800

    change pytorch && qt

--- pytorch/run_pytorch.sh
+++ pytorch/run_pytorch.sh
@@ -2,3 +2,15 @@
 
 # 运行 pytorch 容器实例
 docker run -it --name=pytorch --gpus=all -v ~/workspace/pytorch:/workspace pytorch/pytorch:latest
+
+
+## ----------------------------------------------- 字体配置 ----------------------------------------------------------
+# 注意 python3.10 版本，需要修改为对应容器中的版本
+# 将字体复制进去
+docker cp SimHei.ttf pytorch:/opt/conda/lib/python3.10/site-packages/matplotlib/mpl-data/fonts/ttf
+
+# 将字体配置文件复制进去
+docker cp matplotlibrc pytorch:/opt/conda/lib/python3.10/site-packages/matplotlib/mpl-data
+
+# 进入容器后清除缓存
+rm ~/.cache/matplotlib -rf
--- qt/run_qt.sh
+++ qt/run_qt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 运行 qt 开发容器实例（使用 wayland 协议）
+# 运行 qt6.8 开发容器实例（使用 wayland 协议），版本可以在仓库自行选择
 docker run -it \
     --name=test-qt \
     -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
@@ -15,6 +15,8 @@ sudo apt install mesa-common-dev libgl1-mesa-dev
 # wayland 需要
 apt install -y weston
 
+
+## ------------------------------------------------ CMakeLists.txt 配置 ----------------------------------------------------------
 # 如果在构建时出错，则需要在容器中的 CMakeLists.txt 添加：
 set(CMAKE_PREFIX_PATH "/opt/Qt/6.8.0/gcc_64")
 # 或者
