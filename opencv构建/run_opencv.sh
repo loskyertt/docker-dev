06d11ef4ab2efea3aa417165aee79aa7488b45d0
loskyertt <loskyertt0403@gmail.com>
2025-01-01 16:24:27 +0800

    tmp

--- opencv构建/run_opencv.sh
+++ opencv构建/run_opencv.sh
@@ -7,5 +7,4 @@ git clone https://github.com/opencv/opencv.git
 cmake -B build \
       -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_INSTALL_PREFIX=/opt/opencv \
-      -D ENABLE_PRECOMPILED_HEADERS=OFF \
-      -D BUILD_EXAMPLES=ON ..
+      -D ENABLE_PRECOMPILED_HEADERS=OFF
