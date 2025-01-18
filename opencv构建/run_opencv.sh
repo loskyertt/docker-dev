d3e2c54ea633f17fc8106728bcf5cb9f9f4c0761
loskyertt <loskyertt0403@gmail.com>
2025-01-18 21:41:24 +0800

    tmp

--- opencv构建/run_opencv.sh
+++ opencv构建/run_opencv.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 运行容器实例
+docker run -it --name=opencv-dev ubuntu
+
+# 注：如果在容器中无法进行下载（比如使用 apt install），需要添加 --dns 参数
+docker run -it --name=opencv-dev --dns=8.8.8.8 --dns=8.8.4.4 ubuntu
+
 # ---------------------------------------- build OpenCV ----------------------------------------
 # 拉取源码
 git clone https://github.com/opencv/opencv.git
