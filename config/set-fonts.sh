b3653de81485db2d4a584cf768e2a14e32a2d0ef
loskyertt <loskyertt0403@gmail.com>
2025-01-22 18:09:26 +0800

    tmp

--- config/set-fonts.sh
+++ config/set-fonts.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# 对于 ubuntu
 apt install -y language-pack-zh-hans
 
-## 在 shell 配置文件中添加
-# export LANG=zh_CN.UTF-8
-# export LANGUAGE=zh_CN:zh
-# export LC_ALL=zh_CN.UTF-8
+# 对于 debian
+apt install locales
+dpkg-reconfigure locales
+
+# 在 shell 配置文件中添加
+export LANG=zh_CN.UTF-8
+export LANGUAGE=zh_CN:zh
+export LC_ALL=zh_CN.UTF-8
