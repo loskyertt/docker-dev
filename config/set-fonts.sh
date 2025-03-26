7c0d547cd140dc136bd036d418675543a19fd132
loskyertt <loskyertt0403@gmail.com>
2025-03-26 17:17:25 +0800

    修改 config/set-fonts.sh

--- config/set-fonts.sh
+++ config/set-fonts.sh
@@ -7,6 +7,10 @@ apt install -y language-pack-zh-hans
 apt install locales
 dpkg-reconfigure locales
 
+# 或者编辑 /etc/locale.gen 文件
+nano /etc/locale.gen
+locale-gen
+
 # 在 shell 配置文件中添加
 export LANG=zh_CN.UTF-8
 export LANGUAGE=zh_CN:zh
